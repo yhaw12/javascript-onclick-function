7eb328b4d80261cb7511a38e3671be4654e4b221
yhaw12 <yooxing11@gmail.com>
2022-11-27 14:30:27 +0000

    javascript commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// let smImg = document.getElementById('small-img');
+// let bgImg = document.getElementById('big-img');
+
+
+
+// smImg[0].onclick = function(){
+//     bgImg.src = smImg[0];
+// }
+// smImg[1].onclick = function(){
+//     bgImg.src = smImg[1];
+// }
+// smImg[2].onclick = function(){
+//     bgImg.src = smImg[2];
+// }
+// smImg[3].onclick = function(){
+//     bgImg.src = smImg[3];
+// }
+
+
+
+function changeImg(any){
+
+    document.querySelector('#big-img').src = any;
+
+}
